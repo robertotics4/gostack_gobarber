46a980c9e7ed4a764294ffaaa2441292f37ff966
robertotics4 <robertooliveiragp@gmail.com>
2020-12-09 13:39:50 -0300

    eslint configured (backend)

--- backend/prettier.config.js
+++ backend/prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    trailingComma: "es5",
-    tabWidth: 4,
+    trailingComma: "all",
     semi: true,
     singleQuote: true,
+    arrowParens: 'avoid',
 };
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -18,21 +18,21 @@ app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-        if (err instanceof AppError) {
-            return response.status(err.statusCode).json({
-                status: 'error',
-                message: err.message,
-            });
-        }
-
-        return response.status(500).json({
-            status: 'error',
-            message: 'Internal server error',
-        });
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
     }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
 );
 
 app.listen(3333, () => {
-    console.log('🚀️ Server started on port 3333');
+  console.log('🚀️ Server started on port 3333');
 });
