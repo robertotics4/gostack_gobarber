f8002a55ce15fccbb259ebf4c68d5846dee2c6be
robertotics4 <robertooliveiragp@gmail.com>
2021-01-14 22:56:20 -0300

    methods for provider availability finalized

--- backend/src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ backend/src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -1,35 +1,42 @@
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    CreateDateColumn,
-    UpdateDateColumn,
-    ManyToOne,
-    JoinColumn,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
 @Entity('appointments')
 class Appointment {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    provider_id: string;
+  @Column()
+  provider_id: string;
 
-    @ManyToOne(() => User)
-    @JoinColumn({ name: 'provider_id' })
-    provider: User;
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
-    @Column('time with time zone')
-    date: Date;
+  @Column()
+  user_id: string;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 
-    @UpdateDateColumn()
-    updated_at: Date;
+  @Column('time with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
