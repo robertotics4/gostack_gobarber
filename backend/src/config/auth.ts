f95ee92e9639c521070b38749e40d6954f3a2313
robertotics4 <robertooliveiragp@gmail.com>
2021-01-18 14:54:26 -0300

    created the notification service

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-    jwt: {
-        secret: '0768fbe269d047e3d128ffff82cb3533',
-        expiresIn: '1d',
-    },
+  jwt: {
+    secret: '0768fbe269d047e3d128ffff82cb3533',
+    expiresIn: '1d',
+  },
 };
--- backend/src/modules/users/services/ResetPasswordService.ts
+++ backend/src/modules/users/services/ResetPasswordService.ts
@@ -1,8 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 import { isAfter, addHours } from 'date-fns';
 
-import User from '@modules/users/infra/typeorm/entities/User';
-
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
